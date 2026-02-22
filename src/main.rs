0cf05e9d384aaf60c558d3dc028ccafe6d998126
Viktor Mueller <vr-mueller@tutamail.com>
2026-02-22 14:14:25 +0100

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,40 @@ use duct::cmd;
 use std::env;
 use std::fs;
 use std::path::Path;
+use std::str::FromStr;
 use text_colorizer::*;
 
 mod data;
 mod printer;
+
+enum Command {
+    Add,
+    Restore,
+    Clear,
+    Empty,
+    Help,
+    Show,
+}
+
+impl FromStr for Command {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "add" => Ok(Command::Add),
+            "restore" => Ok(Command::Restore),
+            "clear" => Ok(Command::Clear),
+            "empty" => Ok(Command::Empty),
+            "help" => Ok(Command::Help),
+            "show" => Ok(Command::Show),
+            _ => Err(format!(
+                "{} '{}' is not a valid command.",
+                "Error:".red().bold(),
+                s
+            )),
+        }
+    }
+}
 
 fn main() {
     let home_dir = env::var("HOME").expect("Could not find HOME directory.");
@@ -33,24 +63,7 @@ fn main() {
         // println!("File already exists: {}", trash_info_location);
     }
 
-    match parse_args(&trash_location, &trash_info_location) {
-        Some(args) => match args.option.as_str() {
-            // Add new arguments here
-            "add" => add_file_to_trash(&args.filename, &trash_location, &trash_info_location),
-            "restore" => restore_file(&args.filename, &trash_location, &trash_info_location),
-            "clear" => clear_trash(&args.filename, &trash_location, &trash_info_location),
-            "show" => show_file_list(&trash_info_location),
-            _ => {
-                println!(
-                    "{} Unknown argument '{}'",
-                    "Error:".red().bold(),
-                    args.option.as_str()
-                );
-                printer::print_usage()
-            }
-        },
-        None => {}
-    }
+    parse_args(&trash_location, &trash_info_location);
 }
 
 /// Restores a file from the trash can directory
@@ -87,6 +100,7 @@ fn add_file_to_trash(filename: &str, trash_location: &str, trash_info_location: 
     }
 }
 
+/// List files stored in trash directory by reading
 fn show_file_list(trash_info_location: &str) {
     //TODO: Can be made prettier
     println!("NAME                  LOCATION");
@@ -130,52 +144,85 @@ fn empty_trash(trash_location: &str, trash_info_location: &str) {
     }
 }
 
+/*
 #[derive(Debug)]
 struct Arguments {
     option: String,
     filename: String,
 }
+*/
 
-fn parse_args(trash_location: &str, trash_info_location: &str) -> Option<Arguments> {
-    //TODO: Needs cleaning
-    let args: Vec<String> = env::args().skip(1).collect();
-
-    if args.len() == 1 && args[0] == "help" {
-        // TODO: add help which prints all options
-        println!("Help");
-        return None;
-    } else if args.len() == 1 && args[0] == "show" {
-        return Some(Arguments {
-            option: args[0].clone(),
-            filename: "".to_string(),
-        });
-    } else if args.len() >= 2 && (args[0] == "add" || args[0] == "restore") {
-        let mut i: usize = 1;
-        while i < args.len() {
-            let args = Arguments {
-                option: args[0].clone(),
-                filename: args[i].clone(),
-            };
-            if &args.option == "restore"{
-                restore_file(&args.filename, &trash_location, &trash_info_location);
-            } else {
-                add_file_to_trash(&args.filename, &trash_location, &trash_info_location);
-            }
-            i += 1;
-        }
-        return None;
-    } else if args.len() != 2 {
-        printer::print_usage();
-        eprintln!(
-            "{} wrong number of arguments: expected 2, got {}.",
-            "Error:".red().bold(),
-            args.len()
-        );
-        std::process::exit(1);
+fn eval_single_argument(command: Command, trash_location: &str, trash_info_location: &str) {
+    match command {
+        Command::Help => println!("Help"), // TODO: add help which prints all options
+        Command::Show => show_file_list(&trash_info_location),
+        Command::Empty => empty_trash(&trash_location, &trash_info_location),
+        _ => invalid_arguments(2 as usize, 1 as usize)
     }
+}
 
-    Some(Arguments {
-        option: args[0].clone(),
-        filename: args[1].clone(),
-    })
+fn eval_function(
+    func: fn(&str, &str, &str),
+    input: &Vec<String>,
+    trash_location: &str,
+    trash_info_location: &str,
+) {
+    let mut i: usize = 1;
+    while i < input.len() {
+        let filename: &str = &input[i].clone();
+        func(&filename, &trash_location, &trash_info_location);
+        i += 1;
+    }
+}
+
+fn eval_multi_argument(
+    command: Command,
+    input: &Vec<String>,
+    trash_location: &str,
+    trash_info_location: &str,
+) {
+    match command {
+        Command::Add => eval_function(
+            add_file_to_trash,
+            input,
+            trash_location,
+            trash_info_location,
+        ),
+        Command::Restore => eval_function(restore_file, input, trash_location, trash_info_location),
+        Command::Clear => eval_function(clear_trash, input, trash_location, trash_info_location),
+        _ => invalid_arguments(1 as usize, input.len()),
+    }
+}
+
+fn invalid_arguments(expected: usize, got: usize) {
+    printer::print_usage();
+    eprintln!(
+        "{} wrong number of arguments: expected {}, got {}.",
+        "Error:".red().bold(),
+        expected,
+        got
+    );
+    std::process::exit(1);
+}
+
+fn parse_args(trash_location: &str, trash_info_location: &str) {
+    let args: Vec<String> = env::args().skip(1).collect();
+    if args.len() < 1 {
+        invalid_arguments(1 as usize, args.len());
+        return;
+    }
+    let command_str = &args[0];
+    let command = match command_str.parse::<Command>() {
+        Ok(cmd) => cmd,
+        Err(err) => {
+            eprintln!("{}", err);
+            std::process::exit(1);
+        }
+    };
+
+    match args.len() {
+        1 => eval_single_argument(command, trash_location, trash_info_location),
+        2.. => eval_multi_argument(command, &args, trash_location, trash_info_location),
+        n => invalid_arguments(1 as usize, n),
+    }
 }
